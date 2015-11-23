f838f0c0f58c03c2e4cec4dca452172136034326
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-23 18:54:21 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,9 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                id: 'ID',
+                userId: 'gmail_mail@gmail.com',
+                uploadType: 'media'
             }
         }
     }
--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+var _ = require('lodash'),
+  google = require('googleapis'),
+  OAuth2 = google.auth.OAuth2;
+
+var requireParams = ['id', 'uploadType', 'userId'];
+var pickReqAttr = [
+    'id',
+    'threadId',
+    'labelIds',
+    'snippet',
+    'historyId',
+    'payload'
+];
 module.exports = {
+    checkAuthOptions: function (step, dexter) {
+        _.map(requireParams, function (reqParam) {
+            if(!step.input(reqParam).first()) {
+
+                this.fail('A ' + reqParam +' input variable is required for this module');
+            }
+        }, this);
+
+        if(!dexter.environment('access_token')) {
+
+            this.fail('A access_code environment variable is required for this module');
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +33,24 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        this.checkAuthOptions(step, dexter);
+
+        var oauth2Client = new OAuth2();
+        oauth2Client.setCredentials({access_token: dexter.environment('access_token'), refresh_token: dexter.environment('refresh_token')});
+
+        google.options({ auth: oauth2Client });
+        google.gmail('v1').users.drafts.send({
+            auth: oauth2Client,
+            uploadType: step.input('uploadType', null).first(),
+            userId: step.input('userId', null).first(),
+            resource: {
+                id: step.input('id').first()
+            }
+        }, function (err, message) {
+
+            err? this.fail(err) : this.complete(_.pick(message, pickReqAttr));
+        }.bind(this));
+
     }
 };
