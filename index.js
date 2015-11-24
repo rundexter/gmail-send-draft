dee89d293ae4836f20dac9713373a3147010a428
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-24 17:37:40 +0200

    refactore

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ module.exports = {
             }
         }, this);
 
-        if(!dexter.environment('access_token')) {
+        if(!dexter.environment('google_access_token')) {
 
-            this.fail('A access_code environment variable is required for this module');
+            this.fail('A google_access_code environment variable is required for this module');
         }
     },
 
@@ -37,7 +37,7 @@ module.exports = {
         this.checkAuthOptions(step, dexter);
 
         var oauth2Client = new OAuth2();
-        oauth2Client.setCredentials({access_token: dexter.environment('access_token'), refresh_token: dexter.environment('refresh_token')});
+        oauth2Client.setCredentials({access_token: dexter.environment('google_access_token'), refresh_token: dexter.environment('google_refresh_token')});
 
         google.options({ auth: oauth2Client });
         google.gmail('v1').users.drafts.send({
