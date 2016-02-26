b6d034cc6b30434dd7c574f089c6af1fe3b58bd8
Mike Friedman <friedo@friedo.com>
2016-02-26 13:53:31 -0500

    parallelizing for multiple sends

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 var _ = require('lodash'),
+    Q = require( 'q' ),
     util = require('./util.js'),
     google = require('googleapis'),
     service = google.gmail('v1');
 
-var pickInputs = {
-        'id': { key: 'resource.id', validate: { req: true } },
-        'userId': { key: 'userId', validate: { req: true } },
-        'uploadType': { key: 'uploadType', validate: { req: true, enum: ['media', 'multipart', 'resumable'] } }
-    },
-    pickOutputs = {
-        'id': 'id',
-        'threadId': 'threadId',
-        'labelIds': 'labelIds',
-        'snippet': 'snippet',
-        'historyId': 'historyId',
-        'payload': 'payload'
-    };
+
+var send_msg = function( app, service, user, msg_id, uploadType ) {
+    var deferred = Q.defer();
+    service.users.drafts.send( { 'userId': user, 'uploadType': uploadType, 'resource': { 'id': msg_id } },
+    function( err, response ) {
+        if ( err ) { return deferred.reject( err );        }
+        else       { return deferred.resolve( response );  }
+    } );
+
+    return deferred.promise;
+}
 
 module.exports = {
     /**
@@ -28,21 +27,27 @@ module.exports = {
         var OAuth2 = google.auth.OAuth2,
             oauth2Client = new OAuth2(),
             credentials = dexter.provider('google').credentials();
-        var inputs = util.pickInputs(step, pickInputs),
-            validateErrors = util.checkValidateErrors(inputs, pickInputs);
-
-        if (validateErrors)
-            return this.fail(validateErrors);
 
         // set credential
         oauth2Client.setCredentials({
             access_token: _.get(credentials, 'access_token')
         });
         google.options({ auth: oauth2Client });
-        service.users.drafts.send(inputs, function (err, message) {
 
-            err? this.fail(err) : this.complete(util.pickOutputs(message, pickOutputs));
-        }.bind(this));
+        var ids           = step.input( 'id' );
+        var uploadTypes   = step.input( 'uploadType' );
+        var user          = step.input( 'user' ).first();
 
+        messages = _.zipWith( ids, uploadTypes, function( id, type ) { return { 'id': id, 'type': type } } );
+
+        var sends = [ ];
+        var app = this;
+        messages.forEach( function( item ) {
+            sends.push( send_msg( app, service, user, item.id, item.type ) );
+        } );
+
+        Q.all( sends )
+            .then( function( results ) { app.complete( results ) } )
+            .fail( function( err ) { app.fail( err ) } );
     }
 };
